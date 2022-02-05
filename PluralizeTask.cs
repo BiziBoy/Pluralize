c14e3b707053d8f4b22ccdf316d2caa9f520b265
BiziBoy <79360682+BiziBoy@users.noreply.github.com>
2022-02-05 18:43:33 +0300

    Добавьте файлы проекта.

---
+++ PluralizeTask.cs
@@ -0,0 +1,22 @@
+﻿namespace Pluralize
+{
+	public static class PluralizeTask
+	{
+		public static string PluralizeRubles(int count)
+		{
+			// Напишите функцию склонения слова "рублей" в зависимости от предшествующего числительного count.
+			int ending = (count % 100) > 19 ? count % 10 : count % 100;
+      switch (ending)
+      {
+				case 1:
+					return "рубль";
+				case 2:
+				case 3:
+				case 4:
+					return "рубля";
+        default:
+					return "рублей";
+      }
+    }
+	}
+}
